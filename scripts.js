28f95c0bcc5451756d69a85ecbeb4ceb720941ca
Brian Price <brian.price@student.swic.edu>
2018-12-06 11:46:22 -0600

    Delete old code and add form reset.

--- scripts.js
+++ scripts.js
@@ -22,6 +22,11 @@ const Tool = function Tool(desc, quantity, value, total) {
 const addToolHandler = e => {
   e.preventDefault();
 
+  /**
+   * Pull data for quantity input and convert it to an int.
+   * @param {number} - Pull data from ElementID and convert to int.
+   * @return {number} - Returns quantityInput as integer number.
+   */
   const getQuantityInput = () => {
     const quantityInput = parseInt(
       document.getElementById("quantityInput").value,
@@ -30,6 +35,11 @@ const addToolHandler = e => {
     return quantityInput;
   };
 
+  /**
+   * Pull data for value and converting it to a floating point.
+   * @param {number} - Pull data from ElementID and convert to floating point.
+   * @return {number} - Returns valueInput as Floating Point.
+   */
   const getValueInput = () => {
     const valueInput = parseFloat(
       document.getElementById("valueInput").value,
@@ -47,6 +57,7 @@ const addToolHandler = e => {
     totalInput
   );
 
+  document.getElementById("toolForm").reset();
   tool.push(newTool);
 
   console.log(tool);
@@ -72,42 +83,6 @@ const displayToolData = toolArr => {
     tbody.appendChild(growContentClone);
   });
 };
-
-// /**
-//  * Pull data for the tool input.
-//  * @param {string} - Pull data from ElementID as a string.
-//  * @return {string} - Returns toolInput as string.
-//  */
-// const getToolInput = () => {
-//   const toolInput = document.getElementById("toolInput").value;
-//   return toolInput;
-// };
-
-// /**
-//  * Pull data for quantity input and convert it to an int.
-//  * @param {number} - Pull data from ElementID and convert to int.
-//  * @return {number} - Returns quantityInput as integer number.
-//  */
-// const getQuantityInput = () => {
-//   const quantityInput = parseInt(
-//     document.getElementById("quantityInput").value,
-//     10
-//   );
-//   return quantityInput;
-// };
-
-// /**
-//  * Pull data for value and converting it to a floating point.
-//  * @param {number} - Pull data from ElementID and convert to floating point.
-//  * @return {number} - Returns valueInput as Floating Point.
-//  */
-// const getValueInput = () => {
-//   const valueInput = parseFloat(
-//     document.getElementById("valueInput").value,
-//     10
-//   );
-//   return valueInput;
-// };
 
 // /**
 //  * Create function to push object data into a data Array and reset form.
